3d52166ac485df36f9817b581182c102ea3e99f2
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2025-05-14 19:56:06 +0300

    Binary tree

--- 30_BinaryTree/30_BinaryTree.cpp
+++ 30_BinaryTree/30_BinaryTree.cpp
@@ -1,20 +1,148 @@
-// 30_BinaryTree.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+// sorted binary tree
+class Tree
+{
+	struct Node
+	{
+		// ключ по якому відбувається розміщення елементів
+		int key;
+		// дані, які зберігає елемент
+		int data;
+
+		// вказівник на лівий дочірній елемент
+		Node* left;
+		// вказівник на правий дочірній елемент
+		Node* right;
+
+		Node(int key, int data, Node* left = nullptr, Node* right = nullptr)
+			: key(key), data(data), left(left), right(right)
+		{ }
+	};
+
+	Node* root;
+
+	void Add(int key, int data, Node*& root)
+	{
+		if (root == nullptr)
+		{
+			root = new Node(key, data);
+			return;
+		}
+
+		if (key > root->key)
+			Add(key, data, root->right);
+		else
+			Add(key, data, root->left);
+	}
+
+	// [K]oren [L]ivii [P]ravii
+	void PrintKLP(Node* elem) const
+	{
+		if (elem != nullptr)
+		{
+			// K
+			std::cout << elem->key << ":" << elem->data << " ";
+			// L
+			PrintKLP(elem->left);
+			// P
+			PrintKLP(elem->right);
+		}
+	}
+	// LKP
+	void PrintLKP(Node* elem) const
+	{
+		if (elem != nullptr)
+		{
+			// L
+			PrintLKP(elem->left);
+			// K
+			std::cout << elem->key << ":" << elem->data << " ";
+			// P
+			PrintLKP(elem->right);
+		}
+	}
+	// PKL
+	void PrintPKL(Node* elem) const
+	{
+		if (elem != nullptr)
+		{
+			// P
+			PrintPKL(elem->right);
+			// K
+			std::cout << elem->key << ":" << elem->data << " ";
+			// L
+			PrintPKL(elem->left);
+		}
+	}
+
+	int Search(int key, Node* root) const
+	{
+		if (root == nullptr) return 0;
+
+		if (key > root->key)
+			Search(key, root->right);
+		else if (key < root->key)
+			Search(key, root->left);
+		else
+			return root->data;
+	}
+
+public:
+	Tree() : root(nullptr) { }
+
+	void Add(int key, int data)
+	{
+		Add(key, data, root);
+	}
+
+	int Search(int key) const
+	{
+		return Search(key, root);
+	}
+
+	void PrintKLP() const
+	{
+		PrintKLP(root);
+	}
+	void PrintLKP() const
+	{
+		PrintLKP(root);
+	}
+	void PrintPKL() const
+	{
+		PrintPKL(root);
+	}
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Tree tree;
+
+	tree.Add(50, 200);
+	tree.Add(17, 100);
+	tree.Add(12, 12);
+	tree.Add(23, 5474);
+	tree.Add(19, 54);
+	tree.Add(14, 44);
+	tree.Add(9, 10);
+	tree.Add(72, 30);
+	tree.Add(76, 33);
+	tree.Add(54, 45);
+	tree.Add(67, 11);
+	tree.Add(52, 233);
+	tree.Add(100, 0);
+
+	cout << " KLP : "; tree.PrintKLP(); cout << endl;
+	cout << " LKP : "; tree.PrintLKP(); cout << endl;
+	cout << " PKL : "; tree.PrintPKL(); cout << endl;
+
+	cout << tree.Search(67) << endl;
+
+	system("pause");
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- 31_Inheritance/31_Inheritance.cpp
+++ 31_Inheritance/31_Inheritance.cpp
@@ -2,105 +2,100 @@
 using namespace std;
 
 //private public protected
-class Enemy
-{
+class Enemy {
 protected:
-    string name;
-    int health;
-    float strength;
-    static const int maxHealth = 100;
+	string name;
+	int health;
+	float strength;
+	static const int maxHealth = 100;
 public:
-    Enemy():name("no name"),health(0),strength (0){}
-    Enemy(string name, int s):name( name),health(maxHealth),strength (s){}
-    void Print()
-    {
-        cout << "Name : " << name << endl;
-        cout << "Health : " << health << endl;
-        cout << "Strength : " << strength << endl;
-    }
-    bool IsAlive()
-    {
-        return health > 0;
-    }
-    void DecreaseHealth(int value)
-    {
-        if (health - value < 0)
-            health = 0;
-        else
-            health -= value;
-    }
+	Enemy() :name("no name"), health(0),strength(0){}
+	Enemy(string name, float s) :name(name), health(maxHealth),strength(s){}
+	void Print()
+	{
+		cout << "Name : " << name << endl;
+		cout << "Health : " << health << endl;
+		cout << "Strength : " << strength << endl;
+	}
+	bool IsAlive()
+	{
+		return health > 0;
+	}
+	void DecreaseHealt(int value)
+	{
+		if (health - value < 0)
+			health = 0;
+		else
+			health -= value;
+	}
 };
 //Inheritance
-//class Child : [spetificator]Parent
+//class Child : [spetificator] Parent
 class Dragon : public Enemy
 {
-    float fire;
+	float fire;
 public:
-    Dragon():fire(0), Enemy(){}
-    Dragon(string n, int s, float f):fire(f), Enemy(n,s){}
-   /* {
-        name = "no name";
-        strength = 0;
-        health = 0;
-    }*/
-    void Fight(int damage)
-    {
-        cout << "Getting damage : " << damage << endl;
-        DecreaseHealth(damage - fire);
-        cout << "Health after damage : " << health << " % " << endl;
-    }
-    float Attack()
-    {
-        cout << "Attacking with strength : " << strength << endl;
-        return strength;
-    }
-    void Print()
-    {
-        cout << "------------- Dragon-------------------" << endl;
-        Enemy::Print();
-        cout << "Fire : " << fire << endl;
-    }
-
+	Dragon() :fire(0), Enemy() {}
+	Dragon(string n, float s, float f) :fire(f), Enemy(n,s) {}
+	void Fight(int damage)
+	{
+		cout << "Getting damage : " << damage << endl;
+		DecreaseHealt(damage - fire);
+		cout << "Health after damage : " << health << " % " << endl;
+	}
+	float Attack()
+	{
+		cout << "Attacking with strength : " << strength << endl;
+		return strength;
+	}
+	void Print()
+	{
+		cout << "------------------ Dragon -----------------" << endl;
+		Enemy::Print();
+		cout << "Fire : " << fire << endl;
+	}	
 };
-
 class Monster: public Enemy
 {
-    float run_speed;
+	float runSpeed;
 public:
-    Monster():run_speed(0),Enemy(){}
-    Monster(string n, int s, int r):run_speed(r),Enemy(n,s){}
-    void Print()
-    {
-        cout << "------------- Monster-------------------" << endl;
-        Enemy::Print();
-        cout << "Run speed : " << run_speed << endl;
-    }
-    void Run()
-    {
-        cout << "Run speed : " << run_speed << endl;
-    }
+	Monster():runSpeed(0),Enemy(){}
+	Monster(string n, float s, float r):runSpeed(r),Enemy(n,s){}
+	void Print()
+	{
+		cout << "------------------ Monster -----------------" << endl;
+		Enemy::Print();
+		
+	}
+	void Run()
+	{
+		cout << "Run speed : " << runSpeed << endl;
+	}
+
 };
 int main()
 {
-    Dragon d("Bob",10,5);
-    d.Print();
-    Dragon boss("Boss", 15, 7);
-    boss.Print();
-    int i = 1;
-    do
-    {
-        cout << "------------- Round [ " << i << " ] ---------------" << endl;
-        boss.Fight(d.Attack());
-        d.Fight(boss.Attack());
-        d.Print();
-        boss.Print();
-        i++;
+	Dragon d("Taras",10,5);
+	d.Print();
 
-    } while (d.IsAlive() && boss.IsAlive());
 
-    Monster m("Crazy frog", 2, 150);
-    m.Print();
-    m.Run();
-  
+	Dragon boss("Boss", 15, 7);
+	boss.Print();
+	int i = 1;
+	do
+	{
+		cout << "------------- Round [ " << i << " ] ---------------" << endl;
+		boss.Fight(d.Attack());
+		d.Fight(boss.Attack());
+		d.Print();
+		boss.Print();
+		i++;
+
+	} while (d.IsAlive() && boss.IsAlive());
+	
+	Monster m("Bob",2,150);
+	m.Print();
+	m.Run();
+
 }
 
